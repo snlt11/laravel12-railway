37a024afd0f1460b26fc4542dc5db7bb0f03e55b
Sai Nay Lin Thar <sainaylinthar@gmail.com>
2025-08-13 14:12:45 +0630

    Refactor DatabaseSeeder to create 10,000 users and add testing route for fetching all users

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Models\User;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -12,3 +13,8 @@ Route::get('/user', function (Request $request) {
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+//Just Testing
+Route::get('/users', function () {
+    return User::all();
+})->middleware('auth:sanctum');
